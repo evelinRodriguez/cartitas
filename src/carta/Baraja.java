a2acad44b4cff3677667a91cc5da1262b3fc62a7
pcoronado <paulo_cesar@udistrital.edu.co>
2018-09-13 18:45:07 -0500

    Cartas Versión 0.0.0.4

--- src/carta/Baraja.java
+++ src/carta/Baraja.java
@@ -12,42 +12,35 @@ import java.util.Random;
  * @author Estudiantes
  */
 public class Baraja {
-    
-    String palo[]={"Pica", "Diamante", "Trébol", "Corazón"};    
-    String número[]={"As", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "Jack", "Queen", "King"};
-    Carta[] misCartas=new Carta[52];
-    
-    
-    void crearMazo(){
-        
-        for(int i=0; i<palo.length;i++){
-            for(int j=0; j<número.length;j++){
-            misCartas[(13*i)+j]=new Carta(número[j], palo[i],(i+1)*j);
+
+    String palo[] = {"Pica", "Diamante", "Trébol", "Corazón"};
+    String número[] = {"As", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "Jack", "Queen", "King"};
+    Carta[] misCartas = new Carta[52];
+
+    void crearMazo() {
+
+        for (int i = 0; i < palo.length; i++) {
+            for (int j = 0; j < número.length; j++) {
+                misCartas[j + (i*13)] = new Carta(número[j], palo[i], (i + 1) * j);
+            }
+
         }
-        
     }
-    }
-        
-    void barajar(){
-        Random miRandom=new Random();
-        
-        int pasadas=miRandom.nextInt(1000);
+
+    void barajar() {
+        Random miRandom = new Random();
+
+        int pasadas = miRandom.nextInt(1000);
         Carta miCarta;
-        for(int i=0; i<pasadas;i++){
-            
-            int posiciónInicial=miRandom.nextInt(51);
-            int posiciónFinal=miRandom.nextInt(51);
-            
-            miCarta=misCartas[posiciónInicial];
-            misCartas[posiciónInicial]=misCartas[posiciónFinal];
-            misCartas[posiciónFinal]=miCarta;
+        for (int i = 0; i < pasadas; i++) {
+
+            int posiciónInicial = miRandom.nextInt(51);
+            int posiciónFinal = miRandom.nextInt(51);
+
+            miCarta = misCartas[posiciónInicial];
+            misCartas[posiciónInicial] = misCartas[posiciónFinal];
+            misCartas[posiciónFinal] = miCarta;
         }
     }
-    
-    
-    
-    
-    
-    
-    
+
 }
